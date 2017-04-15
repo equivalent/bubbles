a18074ba195d0046a8268618bc55cc3b0bd886ef
Tomas Valent <equivalent@eq8.eu>
2017-04-15 18:27:49 +0200

    command queue

--- spec/bubbles_spec.rb
+++ spec/bubbles_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Bubbles do
   it "has a version number" do
     expect(Bubbles::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/lib/command_queue_spec.rb
+++ spec/lib/command_queue_spec.rb
@@ -1,4 +1,58 @@
 require 'spec_helper'
-RSpec.describe Bubbles::CommandQueue do
+class Bubbles::CommandQueue
+  def queue
+    @queue ||= []
+  end
 
+  def <<(command_object)
+    queue << command_object
+  end
+
+  def call_next
+    queue.shift.call
+  end
+end
+
+RSpec.describe Bubbles::CommandQueue do
+  subject { described_class.new }
+  let(:command_object) { double 'command_object1' }
+
+  describe '#queue' do
+    it do
+      expect(subject.queue).to be_kind_of(Array)
+    end
+  end
+
+  describe '#<<' do
+    def trigger; subject << command_object end
+
+    it 'shauld add object to queue' do
+      trigger
+      expect(subject.queue).to include(command_object)
+    end
+  end
+
+  describe '#call_next' do
+    def trigger; subject.call_next end
+    let(:command_object2) { double  'command_object2' }
+
+    before do
+      subject << command_object
+      subject << command_object2
+    end
+
+    it do
+      expect(command_object).to receive(:call).with(no_args).once
+      expect(command_object2).not_to receive(:call)
+      trigger
+    end
+
+    context 'run twice' do
+      it do
+        expect(command_object).to receive(:call).with(no_args).once
+        expect(command_object2).to receive(:call).with(no_args).once
+        2.times { trigger }
+      end
+    end
+  end
 end
