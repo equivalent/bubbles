c48c14696d80a401349c3ca5d0b225b6ec66e328
Tomas Valent <equivalent@eq8.eu>
2017-04-16 14:00:39 +0200

    comand queue

--- spec/lib/uploaders/local_dir_spec.rb
+++ spec/lib/uploaders/local_dir_spec.rb
@@ -1,12 +1,8 @@
 require 'spec_helper'
 RSpec.describe Bubbles::Uploaders::LocalDir do
   include_context 'common uploader initialization'
-  let(:command_queue) { [] }
+  let(:command_queue) { [:already_existing_one] }
   let(:bfile)    { instance_double(Bubbles::BubbliciousFile, uid_file: uid_file) }
-  let(:uid_file) do
-    FileUtils.cp(TestHelpers.dummy_file_test1, TestHelpers.dummy_file_test1_processing_dir)
-    TestHelpers.dummy_file_test1_processing_dir
-  end
   let(:config) do
     Bubbles::Config.new.tap do |c|
       c.local_dir_uploader_path = TestHelpers.dummy_local_dir_uploader_dir
@@ -15,20 +11,36 @@ RSpec.describe Bubbles::Uploaders::LocalDir do
 
   describe '#call' do
     def trigger; subject.call end
-    def copied_file
-      Pathname.new(TestHelpers.dummy_local_dir_uploader_dir).join('test1.jpg')
+
+    context 'directory is added' do
+
     end
 
-    it 'keeps the file in processing directory after operation' do
-      expect(File.exist?(uid_file)).to be true
-      trigger
-      expect(File.exist?(uid_file)).to be true
-    end
+    context 'successful file transfer' do
+      let(:uid_file) do
+        FileUtils.cp(TestHelpers.dummy_file_test1, TestHelpers.dummy_file_test1_processing_dir)
+        TestHelpers.dummy_file_test1_processing_dir
+      end
 
-    it 'adds the file to LocalDir Uploader destination' do
-      expect(copied_file).not_to be_file
-      trigger
-      expect(copied_file).to be_file
+      def copied_file
+        Pathname.new(TestHelpers.dummy_local_dir_uploader_dir).join('test1.jpg')
+      end
+
+      it 'keeps the file in processing directory after operation' do
+        expect(File.exist?(uid_file)).to be true
+        trigger
+        expect(File.exist?(uid_file)).to be true
+      end
+
+      it 'adds the file to LocalDir Uploader destination' do
+        expect(copied_file).not_to be_file
+        trigger
+        expect(copied_file).to be_file
+      end
+
+      it 'should not reschedule' do
+        expect { trigger }.not_to change { command_queue.size }
+      end
     end
   end
 end
